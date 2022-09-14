e349f1d471f755b2681c81a389e323ed058695ca
Jérôme LOYET <822436+fatpat@users.noreply.github.com>
2022-09-14 10:46:27 +0200

    [manager] fix footer

--- frontend-manager/src/@core/layouts/components/shared-components/footer/FooterContent.js
+++ frontend-manager/src/@core/layouts/components/shared-components/footer/FooterContent.js
@@ -3,13 +3,13 @@ import Box from '@mui/material/Box'
 import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
 import useMediaQuery from '@mui/material/useMediaQuery'
+import themeConfig from 'src/configs/themeConfig'
 
 const FooterContent = () => {
-  
   return (
     <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
       <Typography sx={{ mr: 2 }}>
-        {`© ${new Date().getFullYear()} - Acropyx`}
+        {`© ${new Date().getFullYear()} - ${themeConfig.templateName}`}
       </Typography>
     </Box>
   )
